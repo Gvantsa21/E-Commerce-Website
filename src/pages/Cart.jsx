a32c8d9d9e2e38f813298b5454f6eff51951aa37
GVANTSA <Churadze.gvantsa@kiu.edu.ge>
2025-07-03 15:14:25 +0400

    last update

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,3 @@
-// Cart.jsx
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { getCartItems, saveCartItems, getCurrency } from "../utils/storage";
